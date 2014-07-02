67592e06a21b96c06bfbd362d7b632c2220dd7d8
Austin Guest <guest.austin@gmail.com>
2014-07-02 18:17:34 -0400

    add Email, PhoneNumber, and Location models

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,35 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20140702190042) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "email_addresses", force: true do |t|
+    t.boolean  "primary"
+    t.string   "value"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "locations", force: true do |t|
+    t.string   "address"
+    t.decimal  "lat",          precision: 10, scale: 6
+    t.decimal  "decimal",      precision: 10, scale: 6
+    t.decimal  "lng",          precision: 10, scale: 6
+    t.string   "borough"
+    t.string   "neighborhood"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "phone_numbers", force: true do |t|
+    t.string   "type"
+    t.boolean  "primary"
+    t.string   "value"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
