a56fedd14db6909c3d9963a764542ae7c89aa6f2
Austin Guest <guest.austin@gmail.com>
2014-07-26 15:42:01 -0400

    Add full set of test cases to specs for .conflicts_with? and .double_books_with?

--- spec/models/assignment_spec.rb
+++ spec/models/assignment_spec.rb
@@ -67,41 +67,47 @@ describe Assignment do
     let(:just_after_shift) do
       FactoryGirl.create( :shift, :without_restaurant, start: shift.end, :end => shift.end + 5.hours )       
     end
+    let(:day_before_shift) do
+      FactoryGirl.create( :shift, :without_restaurant, start: shift.start - 1.day, :end => shift.end - 1.day )     
+    end
+    let(:day_after_shift) do
+      FactoryGirl.create( :shift, :without_restaurant, start: shift.start + 1.day, :end => shift.end + 1.day )       
+    end
 
-    let(:test_shifts){ [ same_time_shift, early_overlap_shift, late_overlap_shift, just_before_shift, just_after_shift ] }
+    let(:test_shifts){ [ same_time_shift, early_overlap_shift, late_overlap_shift, just_before_shift, just_after_shift, day_before_shift, day_after_shift ] }
 
     describe "conflicts_with?(conflicts)" do
 
-      let(:same_day_conflict) do
-        Conflict.create!(
-          rider_id: rider.id,
-          start: shift.start,
-          :end => shift.end )
+      let(:same_time_conflict) do
+        FactoryGirl.create( :conflict, :without_rider, start: shift.start, :end => shift.end )
       end
-      let(:different_day_conflict) do
-        Conflict.create!(
-          rider_id: rider.id,
-          start: shift.start + 1.day,
-          :end => shift.end + 1.day )
-      end
+      let(:early_overlap_conflict) do
+        FactoryGirl.create( :conflict, :without_rider, start: shift.start - 3.hours, :end => shift.end - 3.hours )
+      end     
+      let(:late_overlap_conflict) do
+        FactoryGirl.create( :conflict, :without_rider, start: shift.start + 3.hours, :end => shift.end + 3.hours )
+      end     
       let(:just_before_conflict) do
-        Conflict.create!(
-          rider_id: rider.id,
-          start: shift.start - 5.hours,
-          :end => shift.start )
+        FactoryGirl.create( :conflict, :without_rider, start: shift.start - 5.hours, :end => shift.start )
       end
-      let(:just_after_conflcit) do
-        Conflict.create!(
-          rider_id: rider.id,
-          start: shift.end,
-          :end => shift.end + 5.hours )
+      let(:just_after_conflict) do
+        FactoryGirl.create( :conflict, :without_rider, start: shift.end, :end => shift.end + 5.hours )
+      end
+      let(:day_before_conflict) do
+        FactoryGirl.create( :conflict, :without_rider, start: shift.start - 1.day, :end => shift.end - 1.day )
+      end      
+      let(:day_after_conflict) do
+        FactoryGirl.create( :conflict, :without_rider, start: shift.start + 1.day, :end => shift.end + 1.day )
       end
 
       it "should correctly identify conflicts" do
-        expect( new_assignment.conflicts_with?( [ same_day_conflict ] ) ).to eq true 
-        expect( new_assignment.conflicts_with?( [ different_day_conflict ] ) ).to eq false  
+        expect( new_assignment.conflicts_with?( [ same_time_conflict ] ) ).to eq true 
+        expect( new_assignment.conflicts_with?( [ early_overlap_conflict ] ) ).to eq true  
+        expect( new_assignment.conflicts_with?( [ late_overlap_conflict ] ) ).to eq true                
         expect( new_assignment.conflicts_with?( [ just_before_conflict ] ) ).to eq false
-        expect( new_assignment.conflicts_with?( [ just_after_conflcit ] ) ).to eq false  
+        expect( new_assignment.conflicts_with?( [ just_after_conflict ] ) ).to eq false 
+        expect( new_assignment.conflicts_with?( [ day_before_conflict ] ) ).to eq false 
+        expect( new_assignment.conflicts_with?( [ day_after_conflict ] ) ).to eq false 
       end
     end
 
@@ -110,14 +116,14 @@ describe Assignment do
       before { test_shifts.each { |shift| shift.assign_to rider } }
 
       it "should correctly identify double bookings" do
-
         expect( new_assignment.double_books_with?( [ same_time_shift.assignment ] ) ).to eq true
         expect( new_assignment.double_books_with?( [ early_overlap_shift.assignment ] ) ).to eq true
         expect( new_assignment.double_books_with?( [ late_overlap_shift.assignment ] ) ).to eq true
         expect( new_assignment.double_books_with?( [ just_before_shift.assignment ] ) ).to eq false
         expect( new_assignment.double_books_with?( [ just_after_shift.assignment ] ) ).to eq false
+        expect( new_assignment.double_books_with?( [ day_before_shift.assignment ] ) ).to eq false
+        expect( new_assignment.double_books_with?( [ day_after_shift.assignment ] ) ).to eq false        
       end
-
     end
   end
 end
