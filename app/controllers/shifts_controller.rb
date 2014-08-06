a8cda8725500eae790a3351f4c6e85f5bd32124f
Austin Guest <guest.austin@gmail.com>
2014-08-06 02:35:17 -0400

    add pagination to shifts index

--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -78,41 +78,6 @@ class ShiftsController < ApplicationController
       @rider = Rider.find(params[:rider_id])
     end
 
-    # def load_paths
-    #   case @caller
-    #   when :restaurant
-    #     @paths = {
-    #       show_shift: lambda { |shift| restaurant_shift_path(@restaurant, shift) },
-    #       edit_shift: lambda { |shift| edit_restaurant_shift_path(@restaurant, shift) },
-    #       new_shift: new_restaurant_shift_path,
-    #       index: restaurant_shifts_path(@restaurant),
-    #       show_assignment: lambda { |shift, assignment| restaurant_shift_assignment_path(@restaurant, shift, assignment) },
-    #       edit_assignment: lambda { |shift, assignment| edit_restaurant_shift_assignment_path(@restaurant, shift, assignment) },
-    #       new_assignment: lambda { |shift| new_restaurant_shift_assignment_path(@restaurant, shift) }
-    #     }
-    #   when :rider
-    #     @paths = {
-    #       show_shift: lambda { |shift| rider_shift_path(@rider, shift) },
-    #       edit_shift: lambda { |shift| edit_rider_shift_path(@rider, shift) },
-    #       new_shift: new_rider_shift_path,
-    #       index: rider_shifts_path(@rider),
-    #       show_assignment: lambda { |shift, assignment| rider_shift_assignment_path(@rider, shift, assignment) },
-    #       edit_assignment: lambda { |shift, assignment| edit_rider_shift_assignment_path(@rider, shift, assignment) },
-    #       new_assignment: lambda { |shift| new_rider_shift_assignment_path(@rider, shift) }
-    #     }
-    #   when nil
-    #     @paths = {
-    #       show_shift: lambda { |shift| shift_path(shift) },
-    #       edit_shift: lambda { |shift| edit_shift_path(shift) },
-    #       new_shift: new_shift_path,
-    #       index: shifts_path,
-    #       show_assignment: lambda { |shift, assignment| shift_assignment_path(shift, assignment) },
-    #       edit_assignment: lambda { |shift, assignment| edit_shift_assignment_path(shift, assignment) },
-    #       new_assignment: lambda { |shift| new_shift_assignment_path(shift) }          
-    #     }
-    #   end
-    # end
-
     def load_form_args
       case @caller
       when :restaurant
@@ -127,12 +92,12 @@ class ShiftsController < ApplicationController
     def load_shifts
       case @caller
       when :restaurant
-        @shifts = Shift.where(restaurant_id: @restaurant.id)
+        @shifts = Shift.where(restaurant_id: @restaurant.id).page(params[:page]).order('start ASC')
       when :rider
-        @shifts = Shift.joins(:assignment).where("assignments.rider_id = ?", @rider.id)
+        @shifts = Shift.joins(:assignment).where("assignments.rider_id = ?", @rider.id).page(params[:page]).order('start ASC')
       when nil
         if credentials == 'Staffer'
-          @shifts = Shift.all
+          @shifts = Shift.all.page(params[:page]).order('start ASC')
         else 
           flash[:error] = "You don't have permssion to view that page"
           redirect_to root_path
