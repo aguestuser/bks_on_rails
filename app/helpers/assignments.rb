fcdffc1f2d2d197794474bbd9a003a5c0b42dc85
Austin Guest <guest.austin@gmail.com>
2014-09-11 17:20:28 -0400

    refactor Assignments object -- add assignment wrapping/unwrapping, sort tracking & restoring

--- app/helpers/assignments.rb
+++ app/helpers/assignments.rb
@@ -2,18 +2,18 @@ class Assignments
   include Hashable
   attr_accessor :fresh, :old, :with_conflicts, :with_double_bookings, :with_obstacles, :without_obstacles, :requiring_reassignment
 
-  def initialize options
-    @old = options[:old] || options[:fresh].clone # Array of Assignments 
+  def initialize options={}
+    @old = options[:old] || options[:fresh].clone # Array of WrappedAssignments 
     # NOTE: above will clone fresh options on first iteration, retain initial value of @old on subsequent (recursive) iterations
-    @fresh = options[:fresh].each { |assignment| assignment.id = nil } || []# Array of Assignments
+    @fresh = options[:fresh] || [] # Array of WrapedAssignments
     # raise options[:fresh].inspect
     # raise ( "OLD ASSIGNMENTS: " + @old.inspect + "NEW ASSIGNMENTS: " + @fresh.inspect )
 
-    @with_conflicts =  options[:with_conflicts] || [] # Arr of Assignments
-    @with_double_bookings =  options[:with_double_bookings] || [] # Arr of Assignments
-    @without_obstacles = options[:without_obstacles] || [] # Arr of Assignments
-    @requiring_reassignment = options[:requiring_reassignment] || [] #Arr of Assignments
-  end 
+    @with_conflicts =  options[:with_conflicts] || [] # Arr of WrapedAssignments
+    @with_double_bookings =  options[:with_double_bookings] || [] # Arr of WrapedAssignments
+    @without_obstacles = options[:without_obstacles] || [] # Arr of WrapedAssignments
+    @requiring_reassignment = options[:requiring_reassignment] || [] #Arr of WrapedAssignments
+  end
 
   def with_obstacles
     @with_conflicts + @with_double_bookings 
@@ -28,14 +28,15 @@ class Assignments
         # (3) @without_obstacles: Arr of Assignments with neither conflicts nor double bookings 
       # clears @fresh
     # output: Assignments Obj
-  
-    @fresh.each do |assignment|
+
+    @fresh.each do |wrapped_assignment|
+      assignment = wrapped_assignment.assignment
       if assignment.conflicts.any?
-        @with_conflicts.push assignment
+        @with_conflicts.push wrapped_assignment
       elsif assignment.double_bookings.any?
-        @with_double_bookings.push assignment
+        @with_double_bookings.push wrapped_assignment
       else
-        @without_obstacles.push assignment
+        @without_obstacles.push wrapped_assignment
       end
     end
     @fresh = []
@@ -52,12 +53,13 @@ class Assignments
       # clears @with_conflicts, @with_double_bookings, returns new state of Assignments Object
     with_obstacles = self.with_obstacles
 
-    with_obstacles.each_with_index do |assignment, i|
+    with_obstacles.each_with_index do |wrapped_assignment, i|
       case decisions[i]
       when 'Accept' # move to @requiring_reassignment
-        self.requiring_reassignment.push assignment 
+        self.requiring_reassignment.push wrapped_assignment 
       when 'Override' # resolve obstacle and move to @without_obstacles
-        self.without_obstacles.push assignment.resolve_obstacle
+        wrapped_assignment.assignment.resolve_obstacle
+        self.without_obstacles.push wrapped_assignment
       end      
     end 
     self.with_conflicts = []
@@ -65,52 +67,84 @@ class Assignments
     self
   end
 
-  #to_params
+  def savable
+    #input: self (implicit) Assignments Obj, @without_obstacles (implicit) Array of WrappedAssignments
+    #does: restores Arr of WrappedAssignments without obstacles to original sort and returns unwrapped Arr of Assignments
+    #output: Array of Assignments
+    savable = @without_obstacles.sort_by{ |wrapped_assignment| wrapped_assignment.index }
+    savable.map(&:assignment)
+  end
 
-  def to_params
-    self.to_json.to_query 'assignments'
-    # self.instance_values.to_query 'assignments'
+  def unwrap_old
+    #input: self (implicit), @old (implicit) Array of WrappedAssignments
+    #output: Array of Assignments
+    @old.map(&:assignment)
+  end
+
+  # def to_params
+  #   self.to_json.to_query 'assignments'
+  # end
+
+  # CLASS METHODS
+
+  def Assignments.wrap assignments
+    assignments.each_with_index.map { |assignment, i| WrappedAssignment.new(assignment, i) }
+  end
+
+  def Assignments.wrap_with_indexes assignments, indexes
+    assignments.each_with_index.map { |assignment, i| WrappedAssignment.new(assignment, indexes[i]) } 
   end
 
   def Assignments.from_params params
     #input Hash of type
-    # { 'fresh': [ Arr of Hashes of type:
-      # { 'id' =>, 'shift_id' = > num}, etc...
-    # raise params.inspect
+      # { 'fresh': [
+      #     {
+      #       id: Num,
+      #       assignment:{
+      #         'id': Num,
+      #         'rider_id': Num,
+      #         'shift_id': Num,
+      #         ...(other Assignment attributes)
+      #       }
+      #     }     
+      #    'id': Num
+      #    ],
+      #   'old': [
+      #     {
+      #       'id': Num,
+      #       'assignment':{
+      #         ...(Assignment attributes)...
+      #       }
+      #     }
+      #   ].... (other Arrays of WrappedAssignment attributes)   
+      # }
+    #does: parses params hash into WrappedAssignments that can be passed as options to initialize an Assignments object
+    #output: Assignments Obj
+
     options = {}
-    params.each do |key, ass_arr|
-      options[key.to_sym] = ass_arr.map do |ass_attrs|
-        Assignment.new(ass_attrs)
-      end
+    params.each do |key, wrapped_attr_arr|
+      index_arr = wrapped_attr_arr.map{ |wrapped_attrs| wrapped_attrs['index'] }
+      attr_arr = wrapped_attr_arr.map{ |wrapped_attrs| wrapped_attrs['assignment'] }
+      assignments = attr_arr.map{ |attrs| Assignment.new(attrs) }
+
+      options[key.to_sym] = Assignments.wrap_with_indexes assignments, index_arr
     end
     Assignments.new(options)
   end
 
-  def Assignments.from_id_params id_params
-    # input: Hash of type
-      # { 
-      #   'fresh' => [Arr of Strs (ids)], 
-      #   'with_conflicts' => [Arr of Strs (ids)], 
-      #   'with_double_bookings' => [Arr of Strs (ids)], 
-      #   'with_obstacles' => [Arr of Strs (ids)], 
-      #   'without_obstacles' => [Arr of Strs (ids)],  
-      #   'requiring_reassignment' => [Arr of Strs (ids)],                        
-      # }
-    # does: parses 
-    # output: 
-    options = {}
-    id_params.each do |key, ids|
-      options[key.to_sym] = ids.map do |id|
-        Assignment.find(id.to_i) 
-      end
-    end
-    Assignments.new(options)      
-  end
-
   def Assignments.decisions_from params
-    #input params[:decisions]
+    #input params[:decisions] (must be present)
     decisions = []
     params.each { |k,v| decisions[k.to_i] = v }
     decisions
+  end
+
+  class WrappedAssignment
+    attr_accessor :assignment, :index
+
+    def initialize assignment, index
+      @assignment = assignment
+      @index = index
+    end
   end
 end
