e5362ef1a1fef06faedc7b1092b03f0f9af054b6
Austin Guest <guest.austin@gmail.com>
2014-10-26 17:06:58 -0400

    uncomment new shifts dropdown

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -35,7 +35,7 @@ BksOnRails::Application.configure do
   config.active_support.deprecation = :stderr
 
   #configure for gmail
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :test
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
