ec1dee74e06880a7c5bfbeef13dfa3129be9711d
Austin Guest <guest.austin@gmail.com>
2014-11-10 16:50:31 -0500

    add 10 sec sleep to Rider#conflict_request

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,9 +79,10 @@ BksOnRails::Application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   #configure for gmail
-  # config.action_mailer.perform_deliveries = true
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
+  
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
