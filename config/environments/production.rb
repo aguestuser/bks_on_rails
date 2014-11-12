c273fa40ea7a3dc56d5d53b0c2612873da09f03e
Austin Guest <guest.austin@gmail.com>
2014-11-12 00:47:41 -0500

    add env root as domain param to mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,7 +86,7 @@ BksOnRails::Application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: "gmail.com",
+    domain: ENV["ROOT"].sub(/\/$/, ''),
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: ENV["GMAIL_USERNAME"],
